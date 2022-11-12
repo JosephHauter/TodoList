097d797f86202672bb51f19ea993a35b370da929
Joseph <58018853+JosephHauter@users.noreply.github.com>
2022-11-12 18:17:29 -0500

    added a local storage for the tasks using a hook called useEffect()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
 // import uuid to get a unique id for each task
 import { v4 as uuidv4 } from "uuid";
+// storgae for tasks
+const LOCAL_STORAGE_KEY = "todoApp.tasks";
+
 function App() {
   // array containing tasks
   const [tasks, updateTasks] = useState([]);
   // grabs user input at real time
   const taskref = useRef();
+  // checks if anything stored locally and updates our usestate hook with previous tasks
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) updateTasks((prevTodos) => [...prevTodos, ...storedTodos]);
+  }, []);
+  // saves our tasks locally
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(tasks));
+  }, [tasks]);
+
   function AddTask(e) {
     const name = taskref.current.value;
     // prevents adding empty string as a task
--- src/Task.js
+++ src/Task.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function task({ task }) {
   return (
     <div>
+      {/* this shows a task with a checkbox next to it */}
       <label>
         <input type="checkbox" checked={task.complete}></input>
         {task.name}
